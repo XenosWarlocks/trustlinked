8a2272a3c28434ab6e1b2c04e39461b9768177ed
XenosWarlock <139981020+XenosWarlock@users.noreply.github.com>
2024-07-13 08:11:55 +0530

    comiting

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import AuthForm from '@/components/auth-form';
 
-const SignUpPage = () => {
+const SignUp = async () => {
   return (
-    <div>page</div>
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-up" />
+    </section>
   )
 }
 
-export default SignUpPage;
+export default SignUp
